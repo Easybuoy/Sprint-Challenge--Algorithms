6ea2cb9eb8c68e33a9bc62a3a048b5aaab239f60
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-11 11:57:28 +0100

    solve robot questions

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -100,7 +100,50 @@ class SortingRobot:
         Sort the robot's list.
         """
 
-        
+        # while self.light_is_on() == False:
+        #     self.set_light_on()
+
+        #     while self.can_move_right():
+        #         self.swap_item()
+        #         self.move_right()
+
+        #         if self.compare_item() == 1:
+        #             print(self._list)
+        #             self.swap_item()
+        #             self.move_left()
+        #             self.swap_item()
+        #             self.set_light_off()
+        #         else:
+        #             self.move_left()
+        #             self.swap_item()
+        #             self.move_right()
+
+        #     while self.can_move_left():
+        #         self.move_left()
+
+        while self.light_is_on() == False:
+            self.set_light_on()
+
+            while self.can_move_left():
+                self.move_left()
+                
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+
+                if self.compare_item() == 1:
+                    print(self._list)
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.set_light_off()
+
+                else:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+
+            
 
         print(self._list)
 
